f6f77764da834010b4fe712c4bebec388b5f0455
saegyun <10133jhg@gmail.com>
2023-06-01 19:34:52 +0900

    add description for running and model downloading bat

---
+++ model_download.bat
@@ -0,0 +1,1 @@
+bitsadmin /transfer "test" http://efrosgans.eecs.berkeley.edu/pix2pix/models-pytorch/edges2shoes.pth %1/pytorch-CycleGAN-and-pix2pix-master/checkpoints/edges2shoes_pretrained/latest_net_G.pth
