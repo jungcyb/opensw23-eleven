1a1efb5d4486bf8d002dedeaf85ec9ea2f0892b6
IKSEONGKU <iksung1289@konkuk.ac.kr>
2023-06-06 14:24:09 +0900

    3

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ import os
 import random
 import string
 import sys
+import shutil
 
 
 app = Flask(__name__)
@@ -17,27 +18,39 @@ def index():
 def upload():
         # 임의의 파일 이름 생성
     file = request.files['image']  # 업로드된 파일 가져오기
-    filename = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8)) + '.jpg'
+    filename = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8)) + '.png'
 
-    #static/img 안에 저장
-    file.save(os.path.join('static/img', filename))
+    cdir = os.getcwd().split('\\')[-1]
+    print(os.getcwd())
+    print(cdir)
+    if cdir == 'opensw23-eleven':
+        ()
+    elif cdir == 'server':
+        os.chdir('..')
+    else:
+        os.chdir('opensw23-eleven')
+    # static/img 안에 저장
+    file.save(os.path.join('server/static/img', filename))
+    # test 파일 안 이미지 폴더에 저장
+    shutil.copy('server/static/img/' + filename, 'pytorch-CycleGAN-and-pix2pix-master/datasets/elevenTest/test')
 
-    #테스트 파일 안의 이미지 폴더에 저장
-    file.save(os.path.join('../pytorch-CycleGAN-and-pix2pix-master/datasets//elevenTest/test/', filename))
-#   file.save(os.path.join('uploads', filename))
-
-    modelRun()
+    modelRun(filename)
     return redirect(url_for('show_image', filename=filename))
 
 
-def modelRun():
+def modelRun(filename):
+    filename = filename.split('.')[0]
+    # directory 위치 변경
+    os.chdir("pytorch-CycleGAN-and-pix2pix-master")
+    # test.py 실행
     os.system("python ..\\pytorch-CycleGAN-and-pix2pix-master\\test.py --dataroot datasets/elevenTest --model test --netG unet_256 --direction BtoA --dataset_mode single --norm batch --name edges2shoes_pretrained --gpu_ids -1")
+    # static/img/result 에 저장
+    shutil.copy('results/edges2shoes_pretrained/test_latest/images/'+filename+'_fake.png', '../server/static/img/result/')
     return 
 
 # 저장된 이미지를 보여주는 엔드포인트
 @app.route('/image/<filename>')
 def show_image(filename):
-    
     filename = '../pytorch-CycleGAN-and-pix2pix-master/result/edges2shoes_pretrained/test_latest/images/'+filename
     return render_template('index.html', filename=filename)
 
