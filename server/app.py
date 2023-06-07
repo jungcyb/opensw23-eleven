c6e7d3af1d957b64e63622e75fd972dc87c173c6
IKSEONGKU <iksung1289@konkuk.ac.kr>
2023-06-07 16:08:36 +0900

    add save drawing canvas, client2server

--- server/app.py
+++ server/app.py
@@ -9,12 +9,72 @@ import shutil
 import base64
 
 app = Flask(__name__)
+# drawing 은 그림파일 저장하는 경로, filename 은 upload 된 그림파일 저장하는 경로
+drawing = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8)) + '.png'
+filename = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8)) + '.png'
 
 @app.route('/')
 def index():
     return render_template('sub.html')
 
+# upload for drawing image
+@app.route('/upload2', methods=['POST'])
+def upload2():
+    cdir = os.getcwd().split('\\')[-1]
+    if cdir == 'opensw23-eleven':
+        ()
+    elif cdir == 'server' or cdir == 'pytorch-CycleGAN-and-pix2pix-master':
+        os.chdir('..')
+    else:
+        os.chdir('opensw23-eleven')
+    print('5')
+    image_data_url = request.json['image_data']
+    # chattgpt 짱
+    # 데이터 URL에서 base64 인코딩된 이미지 부분 추출
+    _, encoded_image = image_data_url.split(',', 1)
 
+    # 패딩 확인 및 필요한 패딩 추가
+    padding = len(encoded_image) % 4
+    if padding > 0:
+        encoded_image += '=' * (4 - padding)
+
+    try:
+        # 이미지 데이터 디코딩
+        image_data = base64.b64decode(encoded_image)
+
+        # 이미지 파일로 저장
+        
+        drawing_fake = drawing.split('.')[0]+'_fake.png'
+        drawing_path = 'server/static/img/drawing_img/'+drawing
+        
+        
+        
+        with open(drawing_path, 'wb') as file:
+            file.write(image_data)
+        shutil.copy('server/static/img/drawing_img/' + drawing, 'pytorch-CycleGAN-and-pix2pix-master/datasets/elevenTest/test')
+        print('이미지 저장 성공')
+
+        # 저장된 이미지 파일 경로 응답으로 전송 <= ? 
+        image_path = os.path.abspath(drawing)
+        modelRun_drawing(drawing)
+        
+        return jsonify({'success':True})
+
+    except base64.binascii.Error as e:
+        print('이미지 저장 실패:', e)
+        return {'error': '이미지 저장 실패'}
+
+
+    return '이미지 데이터가 성공적으로 전송되었습니다.'
+
+# server -> client converted drawing image return 
+# 이거 왜 안되는지 모르겠네여
+@app.route('/get_drawing', methods=['GET'])
+def get_drawing():
+    drawing_file_path = 'static/img/drawing_result/'+drawing.split('.')[0]+'_fake.png'
+    return send_file(drawing_file_path, mimetype='image/jpeg')
+
+# save and convert uploaded image / client -> server
 @app.route('/upload', methods=['POST'])
 def upload():
     cdir = os.getcwd().split('\\')[-1]
@@ -29,26 +89,20 @@ def upload():
     # 이미지 데이터를 디코딩하고 파일로 저장
     
     save_image(image_data, filename=filename)
-    modelRun(filename)
+    modelRun_uploaded(filename)
     
     return jsonify({'success':True})
     return redirect(url_for('show_image', filename=filename))
 
+# server -> client converted upload image return
 @app.route('/get_image', methods=['GET'])
 def get_image():
+    
+    filename_fake = filename.split('.')[0]+'_fake.png'
     image_file_path = 'static/img/result/'+filename_fake
     return send_file(image_file_path, mimetype='image/jpeg')
-
-@app.route('/upload/<filename>')
-def show_image(filename):
-    print(filename)
-    origin_file = filename
-    convert_file = '/static/img/result/'+filename
-    print(filename)
-    return render_template('sub.html', origin_file=origin_file, convert_file=convert_file)
-#    return redirect(url_for('show', filename=filename))
     
-
+# static/img/upload_img/filename 저장
 def save_image(image_data, filename):
     # 이미지 데이터를 디코딩
     image_data = base64.urlsafe_b64decode(image_data.split(',')[1])
@@ -60,9 +114,8 @@ def save_image(image_data, filename):
     shutil.copy('server/static/img/upload_img/' + filename, 'pytorch-CycleGAN-and-pix2pix-master/datasets/elevenTest/test')
 
     return
-
-def modelRun(filename):
-    print("1")
+# run test.py
+def modelRun_uploaded(filename):
     filename = filename.split('.')[0]
     # directory 위치 변경
     os.chdir("pytorch-CycleGAN-and-pix2pix-master")
@@ -71,10 +124,18 @@ def modelRun(filename):
     # static/img/result 에 저장
     shutil.copy('results/edges2shoes_pretrained/test_latest/images/'+filename+'_fake.png', '../server/static/img/result/')
     return 
+# run test.py
+def modelRun_drawing(drawing):
+    drawing = drawing.split('.')[0]
+    # directory 위치 변경
+    os.chdir("pytorch-CycleGAN-and-pix2pix-master")
+    # test.py 실행
+    os.system("python ..\\pytorch-CycleGAN-and-pix2pix-master\\test.py --dataroot datasets/elevenTest --model test --netG unet_256 --direction BtoA --dataset_mode single --norm batch --name edges2shoes_pretrained --gpu_ids -1")
+    # static/img/drwaing_result 에 저장
+    shutil.copy('results/edges2shoes_pretrained/test_latest/images/'+drawing+'_fake.png', '../server/static/img/drawing_result/')
+    return 
     
 
 if __name__ == '__main__':
     # 서버 실행
-    filename = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8)) + '.png'
-    filename_fake = filename.split('.')[0]+'_fake.png'
-    app.run(debug=True,host='0.0.0.0',port='1010')
+    app.run(debug=True)
